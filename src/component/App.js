18905c87736d985f73c756d6b94f3cf326a9e296
Pjh9220 <h9220@naver.com>
2024-02-02 09:12:13 +0900

    polygon

---
+++ src/component/App.js
@@ -0,0 +1,135 @@
+import React, { useEffect, useState } from 'react';
+import { Map, Polygon , MapMarker } from 'react-kakao-maps-sdk';
+import sidoData from '../json/sig.json';
+import axios from 'axios';
+
+function App() {
+    const [fillColor, setFillColor] = useState([]);
+    const [path, setPath] = useState([]);
+    const [markeron,SetMarkerOn] = useState([]);
+    const [isOpen, setIsOpen] = useState([]);
+
+    useEffect(()=>{
+        getMap();
+        toursearch();
+    }, []);
+    
+    // 백엔드로부터 받은 지역별 혼잡도 데이터
+    const congestionData = {
+        "고양시일산서구": "원활",
+        "여수시": "조금원활",
+        "고흥군": "보통",
+        "충남": "조금혼잡",
+        "인천": "혼잡",
+        "대전": "원활"
+    };
+
+    //경계선 그리는 함수
+    const getMap = () =>{
+        const features = sidoData.features;
+        const paths = features.map(feature => (
+            feature.geometry.coordinates.map(coords => (
+                coords.map(coordinate => ({
+                    lat: coordinate[1],
+                    lng: coordinate[0]}))
+            ))
+        ));
+        setPath(paths);
+        const colors = features.map(feature => getColor(feature.properties.SIG_KOR_NM));
+        setFillColor(colors);
+    }
+
+
+    // 백엔드에서 받은 데이터를 기반으로 각 지역의 색상을 설정하는 함수
+    const getColor = (key) => {
+        const congestionLevel = congestionData[key];
+        
+        // 혼잡도에 따라 색상 설정
+        if (congestionLevel === "원활") {
+            return "green";
+        } else if (congestionLevel === "조금원활") {
+            return "lightgreen";
+        } else if (congestionLevel === "보통") {
+            return "orange";
+        } else if (congestionLevel === "조금혼잡") {
+            return "pink";
+        } else if (congestionLevel === "혼잡") {
+            return "red";
+        } else {
+            return "#fff";
+        }
+    };
+
+
+    const toursearch = async () => {
+        const baseurl = "http://apis.data.go.kr/B551011/KorService1"
+        const endurl = "/areaBasedList1"
+        const key = ''
+        axios.get(baseurl+endurl,{
+            params:{ serviceKey : encodeURI(key), numOfRows:9999, MobileOS : 'ETC',contentTypeId : 12 , MobileApp :"jeogi", _type:"json"}
+        }).then((res) =>{
+            // console.log(res.data)
+        })
+    }
+
+
+    const markers = async () => {
+        let datalist = [];
+        for(let i=1; i<7; i++){
+            const url = `https://data.ex.co.kr/openapi/locationinfo/locationinfoUnit?key=8156177600&type=json&numOfRows=99&pageNo=${i}`
+            axios.get(url).then((res)=>{
+                datalist = [...datalist,...res.data.list];
+                SetMarkerOn(datalist)
+            })
+
+        }
+        
+    }
+
+
+    //Polygon 클릭 시 지역나오는 함수
+    const polygonClick = (index) => {
+        console.log(sidoData.features[index])
+    }
+
+    
+
+    return (
+        <div>
+            <div className='hello' style={{ width: '100vw', height: '100vh' }}>
+                <Map
+                    center={{ lat: 37.566826, lng: 126.9786567 }}
+                    level={9}
+                    style={{ width: '100%', height: '100%' }}
+                >
+                    {path.map((coordinates, index) => (
+                        <Polygon
+                            key={index}
+                            path={coordinates}
+                            strokeWeight={2}
+                            strokeColor="#004c80"
+                            strokeOpacity={0.8}
+                            fillColor={fillColor[index]}
+                            fillOpacity={0.7}
+                            onClick={()=>{polygonClick(index);}}
+                        />
+                    ))} 
+
+                    {markeron && markeron.flat().map((i,index) => (
+                        <MapMarker 
+                            key={index}
+                            position={{
+                                lat: i.yValue,
+                                lng: i.xValue
+                            }}
+                            onClick={()=>{console.log(i.unitCode,i.unitName);}}
+                        >
+                        </MapMarker>
+                    ))}
+                </Map>
+            </div>
+        </div>
+    );
+}
+
+export default App;
